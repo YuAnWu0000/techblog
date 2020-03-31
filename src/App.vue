c96440f19ff99042a57270b27bd41badbc789ee7
Zachary_Wu <andywu03242000@gmail.com>
2020-03-31 23:00:05 +0800

    fontawesome cdn改為npm package --> 3s降為0.5s, google fonts cdn改為local --> 1.3s降為1s

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,6 @@
 </template>
 
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC:100,300,400,500,700&display=swap&subset=chinese-traditional');
-@import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=swap');
-@import url('https://use.fontawesome.com/releases/v5.11.2/css/all.css');
 
 #app {
   font-family: 'Roboto', 'Noto Sans TC';
--- src/main.js
+++ src/main.js
@@ -4,6 +4,21 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'element-ui/lib/theme-chalk/index.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './assets/css/NotoSansTC.css';
+import './assets/css/Roboto.css';
+
+// prismjs code snippet hignlight library
+import 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css';
+import 'prismjs/components/prism-scss.min';
+import 'prismjs/plugins/autolinker/prism-autolinker.min';
+import 'prismjs/plugins/autolinker/prism-autolinker.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.min.js';
+import Prism from 'vue-prism-component';
+
+Vue.component('prism', Prism);
 
 Vue.use(ElementUI);
 
